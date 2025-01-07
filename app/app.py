18338a18af8353d2f7f8a53c3750dade411cf2b7
sivasaileo <learningpractice999@gmail.com>
2025-01-07 16:16:04 +0530

    First commit App code

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, This is Siva sai. Welcome!"
+def about():
+    return "About Page: This is a Flask Application"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
